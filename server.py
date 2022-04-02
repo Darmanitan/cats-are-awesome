49a6f42a2a0afb748c7ad8364265fe4829e3c5b6
ploob <80493238+Darmanitan@users.noreply.github.com>
2022-04-02 17:30:13 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+app.run()
